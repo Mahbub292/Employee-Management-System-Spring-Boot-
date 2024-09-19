37d82b29ca7512cee1cc75dc05d1bfe694dedeaa
Mahbub292 <mmdmahbub292@gmail.com>
2024-09-20 05:44:27 +0600

    add front-end

--- src/main/java/com/example/EmployeeManagementSystem/EmpControler.java
+++ src/main/java/com/example/EmployeeManagementSystem/EmpControler.java
@@ -3,13 +3,11 @@ package com.example.EmployeeManagementSystem;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.persistence.EntityNotFoundException;
+
 
 
 
@@ -123,12 +121,7 @@ public class EmpControler {
         
     }
     
-    @PutMapping("employee/{id}")
-    public String putMethodName(@PathVariable Long id, @RequestBody Employee employee) {
-        //TODO: process PUT request
-        
-        return empService.update(employee, id);
-    }
+
     
 
 }
